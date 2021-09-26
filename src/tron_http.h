ddb90baa241557d56a97bfd2b05c8793aa3c5b72
Edson Holanda <edsonsobralense@gmail.com>
2021-09-26 02:59:40 -0300

    add tron_http.c and .h

---
+++ src/tron_http.h
@@ -0,0 +1,29 @@
+#ifndef __TRON_HTTP__
+#define __TRON_HTTP__
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+#include "cJSON.h"
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+
+#include "esp_tls.h"
+#include "esp_crt_bundle.h"
+#include "esp_http_client.h"
+
+#define MAX_HTTP_OUTPUT_BUFFER 8192
+
+void https_with_hostname_path();
+
+#endif
